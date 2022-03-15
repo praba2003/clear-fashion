4cbc252ce2927153304f8e8fc65a541e4ab5807c
Praba Mogane <latchoumipraba.mogane@edu.devinci.fr>
2022-03-15 20:23:38 +0100

    feat(get-product): get a specific product

--- server/api.js
+++ server/api.js
@@ -20,11 +20,13 @@ app.get('/', (request, response) => {
 /**
 * ENDPOINT 1 - GET /products
 */
+
 app.get('/products', (request, response) => 
 {
   db.getDB();
   db.find().then(elmt => response.send(elmt));
 });
+
 // To test : http://localhost:8092/products
 
 /**
@@ -33,9 +35,12 @@ app.get('/products', (request, response) =>
 app.get('/products/:id', (request, response) => 
 {
   db.getDB();
-  let url = request.url;
-  let elements = url.split('/');
-  let id = elements[elements.length - 1];
+
+  //let url = request.url;
+  //let elements = url.split('/');
+  //let id = elements[elements.length - 1];
+
+  let id = request.params.id
   db.find({'_id': id}).then(elmt => response.send(elmt[0]));
 });
 // To test : http://localhost:8092/products/89f2dc10-a334-5e29-a5f5-3773d819c195
@@ -43,9 +48,26 @@ app.get('/products/:id', (request, response) =>
 /**
 * ENDPOINT 3 - GET /products/search
 */
-app.get('/products/search', (request, response) => 
+app.get('/products//search', (request, response) => 
 {
+  let limit = request.query.limit;
+  let brand = request.query.brand;
+  let price = request.query.price;
+  
+  db.aggregate(
+    [
+      {'$match': { '$and': [ {'brand': brand}, {'price': {'$lte':parseInt(price)}}]}},
+      {'$sort': {'price': 1}},
+      {'$limit': parseInt(limit)}
+    ]
+  ).then(elmt => response.send(
+    {'limit': limit, 
+     'found':elmt.length, 
+     'results':elmt
+  }));
+  
 });
+// To test : http://localhost:8092/products//search?limit=5&brand=loom&price=50
 
 app.listen(PORT);
 
