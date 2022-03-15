532e867f8bcd9c147d5bef0c20619812e86455a7
Praba Mogane <latchoumipraba.mogane@edu.devinci.fr>
2022-03-15 19:21:53 +0100

    feat(get-product): get a specific product

--- server/api.js
+++ server/api.js
@@ -1,10 +1,9 @@
 const cors = require('cors');
 const express = require('express');
 const helmet = require('helmet');
-
 const PORT = 8092;
-
 const app = express();
+const db = require('./db');
 
 module.exports = app;
 
@@ -16,6 +15,36 @@ app.options('*', cors());
 
 app.get('/', (request, response) => {
   response.send({'ack': true});
+});
+
+/**
+* ENDPOINT 1 - GET /products
+*/
+app.get('/products', (request, response) => 
+{
+  db.getDB();
+  db.find().then(elmt => response.send(elmt));
+});
+// To test : http://localhost:8092/products
+
+/**
+* ENDPOINT 2 - GET /products/:id
+*/
+app.get('/products/:id', (request, response) => 
+{
+  db.getDB();
+  let url = request.url;
+  let elements = url.split('/');
+  let id = elements[elements.length - 1];
+  db.find({'_id': id}).then(elmt => response.send(elmt[0]));
+});
+// To test : http://localhost:8092/products/89f2dc10-a334-5e29-a5f5-3773d819c195
+
+/**
+* ENDPOINT 3 - GET /products/search
+*/
+app.get('/products/search', (request, response) => 
+{
 });
 
 app.listen(PORT);
